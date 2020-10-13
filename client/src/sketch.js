b3d37c170690e03e88ff27f657609543cb6decbd
sonny <sonny.kurniawan.yap@gmail.com>
2020-10-13 10:54:33 +0800

    create side bar

--- client/src/sketch.js
+++ client/src/sketch.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import 'flexboxgrid';
 import {SketchField, Tools} from 'react-sketch';
-import DropArea from './dropzone'
+import DropArea from './dropzone';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import color from '@material-ui/core/colors/blueGrey';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar/Toolbar';
+import Typography from '@material-ui/core/Typography/Typography';
+import UndoIcon from '@material-ui/icons/Undo';
+import RedoIcon from '@material-ui/icons/Redo';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SaveIcon from '@material-ui/icons/Save';
+import DownloadIcon from '@material-ui/icons/CloudDownload';
+import Card from '@material-ui/core/Card';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import CardHeader from '@material-ui/core/CardHeader';
+import Collapse from '@material-ui/core/Collapse';
+import CardContent from '@material-ui/core/CardContent';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
 
 const styles = {
+  root: {
+    padding: '3px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: '10px 10px 5px 10px',
+    justifyContent: 'space-around',
+  },
     dropArea: {
         width: '100%',
         height: '10%',
@@ -21,6 +47,9 @@ const styles = {
         borderStyle: 'solid',
         backgroundColor: '#ffdddd',
       },
+      card: {
+        margin: '10px 10px 5px 0'
+      }
 }
 
 class SketchArea extends React.Component {
@@ -38,6 +67,8 @@ class SketchArea extends React.Component {
             stretchedY: false,
             originX: 'left',
             originY: 'top',
+            canUndo: false,
+            canRedo: false,
         }
 
     }
@@ -84,25 +115,112 @@ class SketchArea extends React.Component {
       };
 
      render = () => {
+      const theme = createMuiTheme({
+        typography: {
+          useNextVariants: true,
+        },
+        palette: {
+          primary: { main: color[500] }, // Purple and green play nicely together.
+          secondary: { main: '#11cb5f' }, // This is just green.A700 as hex.
+        },
+      });
         return (
-            <div>
-                <SketchField 
-                        name="sketch"
-                        className="canvas-area"
-                        ref={c => (this._sketch = c)}
-                        width={
-                          this.state.controlledSize ? this.state.sketchWidth : null
-                        }
-                        height={
-                          this.state.controlledSize ? this.state.sketchHeight : null
-                        }
-                        tool={Tools.Rectangle} 
-                        lineColor='black'
-                        lineWidth={3}/>
-                <div>
-                    <DropArea onDropIn={this._onBackgroundImageDrop} />
-                  </div>
-            </div>        
+            <MuiThemeProvider theme={theme}>
+              <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <AppBar title="Sketch Tool" position="static" style={styles.appBar}>
+                    <Toolbar>
+                      <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>Sketch Tool</Typography>
+                      <IconButton
+                        color="primary"
+                        disabled={!this.state.canUndo}
+                        onClick={this._undo}>
+                        <UndoIcon/>
+                      </IconButton>
+                      <IconButton
+                        color="primary"
+                        disabled={!this.state.canRedo}
+                        onClick={this._redo}>
+                        <RedoIcon/>
+                      </IconButton>
+                      <IconButton
+                        onClick={this._save}>
+                        <SaveIcon/>
+                      </IconButton>
+                      <IconButton
+                        onClick={this._download}>
+                        <DownloadIcon/>
+                      </IconButton>
+                      <IconButton
+                        onClick={this._clear}>
+                        <DeleteIcon/>
+                      </IconButton>
+                    </Toolbar>
+                  </AppBar>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-xs-7 col-sm-7 col-md-9 col-lg-9">
+                  <SketchField 
+                          name="sketch"
+                          className="canvas-area"
+                          ref={c => (this._sketch = c)}
+                          width={
+                            this.state.controlledSize ? this.state.sketchWidth : null
+                          }
+                          height={
+                            this.state.controlledSize ? this.state.sketchHeight : null
+                          }
+                          tool={Tools.Rectangle} 
+                          lineColor='black'
+                          lineWidth={3}/>
+                </div>
+
+
+                <div className="col-xs-5 col-sm-5 col-md-3 col-lg-3">
+                  <Card style={styles.card}>
+                    <CardHeader
+                      title="Tools"
+                      subheader="Available tools"
+                      action={
+                        <IconButton
+                          onClick={(e) => this.setState({ expandTools: !this.state.expandTools })}>
+                          <ExpandMore/>
+                        </IconButton>
+                      }/>
+                    <Collapse in={this.state.expandTools}>
+                      <CardContent>
+                        <div className="row">
+                          <div className="col-lg-12">
+                            <TextField
+                              select={true}
+                              label="Canvas Tool"
+                              value={this.state.tool}
+                              onChange={this._selectTool}
+                              helperText="Please select Canvas Tool">
+                              <MenuItem value={Tools.Select} key="Select">Select</MenuItem>
+                              <MenuItem value={Tools.Pencil} key="Pencil">Pencil</MenuItem>
+                              <MenuItem value={Tools.Line} key="Line">Line</MenuItem>
+                              <MenuItem value={Tools.Arrow} key="Arrow">Arrow</MenuItem>
+                              <MenuItem value={Tools.Rectangle} key="Rectangle">Rectangle</MenuItem>
+                              <MenuItem value={Tools.Circle} key="Circle">Circle</MenuItem>
+                              <MenuItem value={Tools.Pan} key="Pan">Pan</MenuItem>
+                              <MenuItem value={Tools.RectangleLabel} key="Pan">RectangleLabel</MenuItem>
+                            </TextField>
+                          </div>
+                        </div>
+                      </CardContent>
+                    </Collapse>
+                  </Card>
+                </div>
+              </div>
+
+              <div>
+                <DropArea onDropIn={this._onBackgroundImageDrop} />
+              </div>
+                  
+            </MuiThemeProvider>        
         )
      }
 }
